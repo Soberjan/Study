efb8a6ab3f1878074300a157d8b56659b5edfb09
Aleksander <operations.rannsoknir@gmail.com>
2018-09-22 09:02:34 +0300

    1

--- C4/custom/1/Optimal/Simple.bat
+++
@@ -1,7 +0,0 @@
-C:\LazarusApp\fpc\3.0.4\bin\x86_64-win64\fpc.exe simple.lpr
-simple.exe < tests\00 > tests\00.out
-fc tests\00.a tests\00.out
-
-
-pause
-
--- C4/custom/1/Optimal/tests_gen.bat
+++
@@ -1,3 +0,0 @@
-C:\LazarusApp\fpc\3.0.4\bin\x86_64-win64\fpc.exe tests_gen.lpr
-
-pause
