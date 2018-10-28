91609e09f8073af9b16bff306b96bc931cbac2fa
Aleksander <operations.rannsoknir@gmail.com>
2018-10-28 17:40:05 +0300

    C4/custom/1/.bat

--- C4/custom/1/simple.bat
+++ C4/custom/1/simple.bat
@@ -1,9 +1,10 @@
 @echo off
+set dst=tests
 fpc.exe %~n0.lpr > %~n0.tmp
-for %%i IN ( tests\*.a ) do (
-%~n0.exe < tests\%%~ni > tests\%%~ni.out 
-fc %%i tests\%%~ni.out
-del tests\%%~ni.out                         
+for %%i IN ( %dst%\*.a ) do (
+%~n0.exe < %dst%\%%~ni > %dst%\%%~ni.out 
+fc %%i %dst%\%%~ni.out
+del %dst%\%%~ni.out                         
 )                                                                                  
-del %~n0.o %~n0.exe %~n0.tmp                        
+del %~n0.o %~n0.exe %~n0.tmp                                         
 pause
