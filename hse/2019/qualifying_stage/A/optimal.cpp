25a6dd0a8b83768315e5f80ef043d412b2cecfaf
Aleksander <operations.rannsoknir@gmail.com>
2018-11-14 18:20:48 +0300

    hse/2019/qualifying_stage/A

--- hse/2019/qualifying_stage/A/optimal.cpp
+++ hse/2019/qualifying_stage/A/optimal.cpp
@@ -9,17 +9,11 @@ int optimal(){
     if (n - k >= x)
         return (n);
 
-    int tasks_amount = n - k;
-    int diff_easy = x / tasks_amount;
-    int diff_hard = diff_easy + 1;
-    int hard_amount = x % tasks_amount;
-    if (hard_amount == 0)
+    int diff_easy = x / (n - k);
+    if (x % (n - k) == 0)
         return n * diff_easy;
-    int easy_amount = tasks_amount - hard_amount;
 
-    int sum = diff_hard * hard_amount + diff_easy * easy_amount + diff_hard * k;
-
-    return sum;
+    return x + k * (diff_easy + 1);
 }
 
 int main()
