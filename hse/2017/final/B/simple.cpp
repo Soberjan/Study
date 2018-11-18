e7e29700f0ea6b7f8b1c41229f412db10101a9d0
Aleksander <operations.rannsoknir@gmail.com>
2018-11-18 09:31:57 +0300

    custom/QuickSort hse/2017/final/B

--- hse/2017/final/B/simple.cpp
+++ hse/2017/final/B/simple.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
 #include <algorithm>
-#include <math.h>
 
 using namespace std;
-const int NLIM = 10e5;
-int n, k, a[NLIM], s;
+const int NLIM = 100, HLIM = 100;
+int n, k, a[NLIM][HLIM], b[NLIM], s;
 
-void divide(int lo, int hi){
-    int maximum = 0;
-    for (int i = lo; i < hi; i++)
-        if (a[i] >= a[maximum])
-            maximum = i;
-    for (int i = maximum; i < hi; i++)
-            s += a[maximum] - a[i];
-    if (maximum > lo)
-        divide(lo, maximum);
+void print(){
+    for (int i = 0; i < NLIM; i++){
+        for (int j = 0; j < HLIM; j++)
+            cout << a[j];
+        cout << "\n";
+        }
     }
 
 void simple(){
-    int maximum = 0;
-    for (int i = 0; i < n; i++)
-        if (a[i] > a[maximum])
-            maximum = i;
+    print();
+    }
 
-    divide(0, maximum);
-    divide(maximum + 1, n);
-}
 
 int main(){
     //freopen("tests\\00", "r", stdin);
 
     cin >> n >> k;
     for (int i = 0; i < n; i++)
-        cin >> a[i];
+        cin >> b[i];
 
     simple();
 
--- hse/2019/qualifying_stage/A/tests_gen.bat
+++
@@ -1,4 +0,0 @@
-fpc.exe tests_gen.lpr
-tests_gen.exe
-del %~n0.o %~n0.exe %~n0.tmp
-pause
