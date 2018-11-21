fba72049b576dde9d0f5420b9515325a6d1b3066
Aleksander <operations.rannsoknir@gmail.com>
2018-11-21 18:58:07 +0300

    open/qualification/C

--- open/qualification/C/optimal.cpp
+++ open/qualification/C/optimal.cpp
@@ -30,9 +30,9 @@ long optimal(){
 int main()
 {
     //freopen("tests\\00", "r", stdin);
-    cin.tie(0);
-    cout.tie(0);
-    ios_base::sync_with_stdio(false);
+//    cin.tie(0);
+//    cout.tie(0);
+//    ios_base::sync_with_stdio(false);
     cin >> n >> p >> k;
     for (int i = 0; i < n; i++)
         cin >> a[i];
