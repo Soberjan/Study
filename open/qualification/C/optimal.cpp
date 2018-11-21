56d2e51342261084afba5dfb7b7759aa54206067
Aleksander <operations.rannsoknir@gmail.com>
2018-11-21 18:30:04 +0300

    open/qualification/C

--- open/qualification/C/optimal.cpp
+++ open/qualification/C/optimal.cpp
@@ -4,39 +4,39 @@
 
 using namespace std;
 
-long n, p, k, a[50];
+long n, k, a[100000];
+long long p;
 
 long optimal(){
-    sort(a, a + n, [](int l, int r){ return (l > k ? l % k : k - l) < (r > k ? r % k : k - r );});
+    int b[100000];
     int s = 0;
     for (int i = 0; i < n; i++){
-        if (p > 0){
-            if (a[i] < k){
-                s += 1;
-                p -= k - a[i];
-            }
-            if (a[i] >= k){
-                if (a[i] / k == 0)
-                    s += a[i] / k;
-                if (a[i] % k > 0){
-                    s += (a[i] + k - a[i] % k) / k;
-                    p -= k - a[i] % k;
-                }
-            }
+        b[i] = a[i] % k;
+        s += a[i] / k;
+    }
+    sort(b, b + n, greater<int>());
+    for (int i = 0; i < n; i++){
+        if (b[i] == 0)
+            break;
+        if (k - b[i] <= p){
+            s += 1;
+            p -= k - b[i];
         }
     }
-    return (p > 0 ? s += p / k : s);
+    s += p / k;
+    return s;
 }
 
 int main()
 {
-    freopen("tests\\00", "r", stdin);
+    //freopen("tests\\00", "r", stdin);
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(false);
     cin >> n >> p >> k;
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    cout << optimal();
+    int ans = optimal();
+    cout << ans;
     return 0;
 }
--- open/qualification/C/tests_gen.cpp
+++ open/qualification/C/tests_gen.cpp
@@ -1,39 +1,60 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 void CreateTest(int i){
-    int n = rand() % 20;
-    int p = rand() % 50;
-    int k = rand() % 30;
-    int a[50];
-    for (int i = 0; i < n; i++)
-        a[i] = rand() % 20;
+    int n = 3;
+    int p = rand() / 100;
+    int k = rand() / 20;
+    int a[10];
+    for (int j = 0; j < n; j++)
+        a[j] = rand() / 30;
 
-    printf("%2d %2d %2d \n", n, p, k);
-    for (int i = 0; i < n; i++)
-        printf("%2d", a[i]);
+    printf("%d %d %d \n", n, p, k);
+    for (int j = 0; j < n; j++)
+        printf("%d ", a[j]);
     printf("\n");
     int ans;
-    scanf("%2d", &ans);
+    scanf("%d", &ans);
     printf("\n");
 
     char buf [50];
     sprintf(buf, "tests\\%.2d", i);
     freopen (buf, "w", stdout);
-    printf("%2d %2d %2d \n", n, p, k);
-    for (int i = 0; i < n; i++)
-        printf("%2d ", a[i]);
+    printf("%d %d %d \n", n, p, k);
+    for (int j = 0; j < n; j++)
+        printf("%d ", a[j]);
     fclose(stdout);
 
     sprintf(buf, "tests\\%.2d.a", i);
     freopen (buf, "w", stdout);
-    printf("%2d", ans);
+    printf("%d", ans);
+    fclose(stdout);
+    }
+
+void CreateTest1(int i){
+    int n = 100000;
+    long long p = 1000000000000000000;
+    int k = 1000000000;
+    int a[100000];
+    for (int j = 0; j < n; j++)
+        a[j] = 1;
+
+    char buf [50];
+    sprintf(buf, "tests\\%.2d", i);
+    freopen (buf, "w", stdout);
+    cout << n << " " << p << " " << k << "\n";
+    for (int j = 0; j < n; j++)
+        cout << a[j] << " ";
     fclose(stdout);
     }
 
 
 int main(){
-    for (int i = 2; i < 6; i++)
-        CreateTest(i);
+    srand(time(0));
+//    for (int i = 7; i < 9; i++)
+//        CreateTest(i);
+    CreateTest1(10);
     return 0;
+
     }
