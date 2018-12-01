d2fbe62dd577aac890ebc72c87f67e888b5a9d04
Aleksander <operations.rannsoknir@gmail.com>
2018-12-01 12:49:35 +0300

    hse/2019/B

--- hse/2019/B/optimal.cpp
+++ hse/2019/B/optimal.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
-#include<stdio.h>
-#include<algorithm>
+#include <map>
 using namespace std;
 
 int n, a[100001];
 
 int optimal(){
+    map<int, int> partic;
+    for (int i = 0; i < n; i++)
+        partic[a[i]] += 1;
 
+    int s = 0;
+    for(map<int,int>::iterator it=partic.begin(); it!=partic.end(); ++it){
+        int x = it->first;
+        int y = it->second;
+        s += y/x*x;
+        //cout << x << " " << y << " " << s << "\n";
+    }
+
+    return s;
 }
 
 int main()
@@ -15,7 +26,6 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    a[n] = 0;
 
     cout << optimal();
     return 0;
