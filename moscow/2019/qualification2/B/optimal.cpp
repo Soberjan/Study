2b5f0d0b04b576d21f571a1020c79a27ca529344
Aleksander <operations.rannsoknir@gmail.com>
2019-01-19 13:42:37 +0300

    moscow/2019/qualification2/B

--- moscow/2019/qualification2/B/optimal.cpp
+++ moscow/2019/qualification2/B/optimal.cpp
@@ -1,15 +1,56 @@
 #include <iostream>
 #include <stdio.h>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <locale>
 using namespace std;
+#define ll long long
 
-string line;
+//double minfloat(vector<string> words){
+//    return (min(min(a[0])))
+//}
+
+//string minword(vector<string> words){
+//return 0;
+//}
+//
+//int minint(vector<string> words){
+//return ni;
+//}
+
+string simple(string line){
+    vector<string> words;
+    istringstream iss(line);
+    string word;
+    while (getline(iss, word, '\t'))
+        words.push_back(word);
+    bool fl, wd;
+    for(int i = 0; i < line.size(), fl==0, wd==0; i++)
+        if (line[i]=='.')
+            fl = 1;
+        else if (isalpha(line[i]))
+            wd = 1;
+//    if (fl)
+//        return minfloat(vector<string> words)
+//    if (wd)
+//        return minword(vector<string> words)
+//    return minint
+}
 
 int main()
 {
     freopen("tests/00", "r", stdin);
-    while (getline(cin, line))
-    {
-        cout << line << endl;
-    }
+    string line;
+//    while (getline(cin, line))
+//        cout << simple(line) << endl;
+//    string str = "afaf  adf as asdf";
+//    string word;
+//    istringstream words(str);
+//    getline(words, word, '\t');
+//    cout << word;
+    string a = "AB";
+    string b = "12";
+    cout << a.compare(b);
     return 0;
 }
