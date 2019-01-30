57eb1244c853ce7b8f079a6a43ea6d1fa715290a
Aleksander <operations.rannsoknir@gmail.com>
2019-01-30 18:56:44 +0300

    moscow/2019/qualification2/C

--- moscow/2019/qualification2/B/super.cpp
+++ moscow/2019/qualification2/B/super.cpp
@@ -1,69 +1,43 @@
+#include <cstdio>
 #include <iostream>
-#include <stdio.h>
-#include <string>
-#include <sstream>
+#include <cstring>
 using namespace std;
 
-string b[3];
-string a;
-int minWord(){
-    int lst = 0;
-    for (int i = 1; i < 3; i++)
-        if (b[i].compare(b[lst]) < 0)
-            lst = i;
-    return lst;
+char* minint(char l[100], char r[100]){
+    bool ms1 = 0, ms2 = 0;
+    ms1 = l[0] == '-' ? 1 : 0;
+    ms2 = r[0] == '-' ? 1 : 0;
+    if (ms1 && ms2)
+        return minint(l + 1, r + 1);
+    if (ms1 || ms2)
+        return ms1 == 1 ? l : r;
+
+    if ((int)strlen(l) == (int)strlen(l))
+        return strcmp(l, r) < 0 ? l : r;
+
+    return (int)strlen(l) < (int)strlen(l) ? l : r;
 }
 
-int minFl(){
-    long double dl[3];
-    for (int i = 0; i < 3; i++)
-        dl[i] = stold(b[i], 0);
-    int lst = 0;
-    for (int i = 1; i < 3; i++)
-        if (dl[i] < dl[lst])
-            lst = i;
-    return lst;
+char* min (char l[100], char r[100]){
+    int dot = 0, alp = 0;
+    for (int i = 0; i < (int)strlen(l); i++){
+        dot = dot || (l[i] == '.');
+        alp = alp || (isalpha(l[i]));
+    }
+    if (alp)
+        return (strcmp(l, r) < 0) ? l : r;
+    if (!dot)
+        return (minint(l, r));
+
 }
 
-int minInt(){
-    long long ll[3];
-    for (int i = 0; i < 3; i++)
-        ll[i] = stoll(b[i], 0, 0);
-    int lst = 0;
-    for (int i = 1; i < 3; i++)
-        if (ll[i] < ll[lst])
-            lst = i;
-    return lst;
-}
-
-int main ()
-{
+int main(){
     //freopen("tests/01", "r", stdin);
-
-    while (getline(cin, a)){
-        istringstream str(a);
-        for (int i = 0; i < 3; i++)
-            getline(str, b[i], '\t');
-//        for (int i = 0; i < 3; i++)
-//            cout << b[i] << ' ';
-//        cout << '\n';
-        bool isAl = 0, isFl = 0;
-        for (int i = 0; ( (i < (int)a.size()) && (!isAl) && (!isFl) ); i++){
-            if (isalpha(a[i]))
-                isAl = 1;
-            if (a[i] == '.')
-                isFl = 1;
-        }
-        int idx;
-        if (isAl)
-            idx = minWord();
-        else if (isFl)
-            idx = minFl();
-        else
-            idx = minInt();
-
-        cout << b[idx] << '\n';
-        }
+    char a[100], b[100], c[100];
+    while (cin >> a >> b >> c){
+        cout << a << " " << b << " " << c << "\n";
+        cout << min(min(a, b), c);
+    }
 
     return 0;
 }
