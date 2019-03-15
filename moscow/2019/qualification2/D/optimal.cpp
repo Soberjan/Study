749c21b82aa02e392ef6013a560c3a56738f5787
Aleksander <operations.rannsoknir@gmail.com>
2019-03-15 21:32:18 +0300

    moscow/2019/qualification2/B

--- moscow/2019/qualification2/D/optimal.cpp
+++ moscow/2019/qualification2/D/optimal.cpp
@@ -1,28 +1,16 @@
 #include <iostream>
-#include <stdio.h>
+#include <cstdio>
 using namespace std;
 
-int a[100000000], n, k;
-
+int n, k;
+int * a = new int[1000000];
 int main()
 {
-    freopen("tests/00", "r", stdin);
-    cin >> n >> k;
+    //freopen("tests/03", "r", stdin);
+    cin >> n >> m;
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    int m = n - k + 1;
-    int z = (m - k) / k;
-    int b[100000000];
-    for (int i = 0, s = 0; i < z; s = 0, i++){
-        for (int j = i * k; j < k; j++)
-            s += a[j];
-        int q = s / k;
-        for (int j = i * k; j < k; j++)
-            b[j] = q;
-    }
-    for (int i = k * z + 1; i < m; i++)
-        b[i] = a[i];
-    for (int i = 0; i < m; i++)
-        cout << a[i];
+
+
     return 0;
 }
---
+++ moscow/2019/qualification2/D/simple.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int n, k, *a = new int [100000];
+
+int main()
+{
+    freopen("tests/01", "r", stdin);
+    cin >> n >> k;
+    int *b = new int [n - k + 1];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n - k + 1; i++){
+        int *c = new int [k];
+        for (int j = 0; j < k; j++)
+            c[j] = a[i + j];
+        sort(c, c + k);
+        b[i] = c[k/2];
+    }
+    for (int i = 0; i < n - k + 1; i++)
+        cout << b[i] << ' ';
+    return 0;
+}
