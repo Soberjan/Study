ac75dce959164236b7fd8d105cf4ad41ce9a0b25
Aleksander <operations.rannsoknir@gmail.com>
2019-03-29 22:10:19 +0300

    containers/vector

---
+++ containers/vector/vector.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct MyVector{
+    int size = 0, capacity = 4;
+    int *a = new int[capacity];
+    void push_back(int i){
+        if (size < capacity)
+            a[size++] = i;
+        else if (size == capacity){
+            int *b = new int[capacity];
+            memcpy(b, a, 4 * capacity);
+            capacity *= 2;
+            delete[] a;
+            a = new int[capacity];
+            memcpy(a, b, 4 * capacity);
+            a[size++] = i;
+        }
+    }
+    void change(int id, int value){
+        a[id] = value;
+    }
+    int get_value(int id){
+        return a[id];
+    }
+};
+
+int main()
+{
+    MyVector v;
+    for (int i = 0; i < 130; i++)
+        v.push_back(i);
+    v.change(0, 123123);
+    for (int i = 0; i < 130; i++)
+        cout << v.get_value(i) << " ";
+    cout << "\n" << v.size << " " << v.capacity;
+    return 0;
+}
