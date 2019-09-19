34d7249d7750ff3c2866e07afdc5a29a3ac4114c
Aleksander <operations.rannsoknir@gmail.com>
2019-09-19 20:24:16 +0300

    containers/deque

--- containers/deque/deque.cpp
+++ containers/deque/deque.cpp
@@ -2,56 +2,60 @@
 //#include <deque>
 using namespace std;
 
-template <typename T>
 struct deque{
-    int chunk_size = 5, chunk_cluster_size = 1;
-    T **chunk_cluster = new T*[chunk_cluster_size];
-    int fst = 0, lst = 1;
-    deque(){
+    int chunk_size = 3, chunk_cluster_size = 1;
+    int **chunk_cluster = new int*[chunk_cluster_size];
+    int fst = 0, lst = 0;
+    deque(int r){
         for (int i = 0; i < chunk_cluster_size; i++)
-            chunk_cluster[i] = new T[chunk_cluster_size];
+            chunk_cluster[i] = new int[chunk_size];
+        chunk_cluster[0][0] = r;
     }
-    int& operator[](int r){ return chunk_cluster[(r + fst) / chunk_size][r + fst - (r + fst)/chunk_size*chunk_size]; }
-    void push_back(T i){
-        if (lst < chunk_size)
-            chunk_cluster[chunk_cluster_size - 1][lst++] = i;
+    int& operator[](int r){
+        int n = r - (chunk_size - fst);
+        int p = n < 0 ? 0 : 1 + n/chunk_size;
+        int q = n < 0 ? fst + r : n % chunk_size;
+        return chunk_cluster[p][q];
+    }
+    void push_back(int i){
+        if (lst < chunk_size - 1){
+            chunk_cluster[chunk_cluster_size - 1][++lst] = i;
+        }
         else{
-            T** tmp = new T*[++chunk_cluster_size];
+            int** tmp = new int*[++chunk_cluster_size];
             for (int j = 0; j < chunk_cluster_size - 1; j++)
                 tmp[j] = chunk_cluster[j];
-            tmp[chunk_cluster_size-1] = new T[chunk_size];
+            tmp[chunk_cluster_size-1] = new int[chunk_size];
             chunk_cluster = tmp;
             lst = 0;
-            chunk_cluster[chunk_cluster_size - 1][lst++] = i;
+            chunk_cluster[chunk_cluster_size - 1][0] = i;
         }
     }
-    void push_front(T i){
-        if (fst >= 0)
-            chunk_cluster[0][fst--] = i;
+    void push_front(int i){
+        if (fst > 0)
+            chunk_cluster[0][--fst] = i;
         else{
-            T** tmp = new T*[++chunk_cluster_size];
+            int** tmp = new int*[++chunk_cluster_size];
             for (int j = 1; j < chunk_cluster_size; j++)
                 tmp[j] = chunk_cluster[j - 1];
-            tmp[0] = new T[chunk_size];
+            tmp[0] = new int[chunk_size];
             chunk_cluster = tmp;
             fst = chunk_size - 1;
-            chunk_cluster[0][fst--] = i;
+            chunk_cluster[0][fst] = i;
         }
     }
     void pop_back(){
-        if (lst >= 0)
-            lst--;
-        else{
+        lst--;
+        if (lst < 0){
             delete[] chunk_cluster[chunk_cluster_size-- - 1];
-            lst = 6;
+            lst = chunk_size - 1;
         }
     }
     void pop_front(){
-        if (fst < chunk_size)
-            fst++;
-        else{
+        fst++;
+        if (fst >= chunk_size){
             delete[] chunk_cluster[0];
-            T** tmp = new int*[--chunk_cluster_size];
+            int** tmp = new int*[--chunk_cluster_size];
             for (int i = 1; i < chunk_cluster_size + 1; i++)
                 tmp[i - 1] = chunk_cluster[i];
             chunk_cluster = tmp;
@@ -61,12 +65,17 @@ struct deque{
 };
 
 int main(){
-    deque<int> deq;
-    for (int i = 0; i < 20; i++)
+    deque deq(0);
+    for (int i = 1; i < 21; i++)
         deq.push_back(i);
-    for (int i = -1; i > -16; i--)
+    for (int i = -1; i > -21; i--)
         deq.push_front(i);
-    for (int i = 0; i < 35; i++)
+    for (int i = 0; i  < 10; i++)
+        deq.pop_back();
+    for (int i = 0; i  < 10; i++)
+        deq.pop_front();
+    deq[0] = 123123;
+    for (int i = 0; i < 21; i++)
         cout << deq[i] << " ";
     return 0;
 }
