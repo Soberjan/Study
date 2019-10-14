824ec9b6730710071945ceb3f4a650470dde1770
Aleksander <operations.rannsoknir@gmail.com>
2019-10-14 20:42:00 +0300

    vseros/2016/lvl1/reference vseros/2016/lvl2/reference vseros/2017/lvl1/ vseros/2018/lvl1/1 vseros/2018/lvl1/2 vseros/2018/lvl1/3 vseros/2018/lvl1/4

---
+++ vseros/2017/lvl1/1/simple.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+long long a, b, c;
+
+int main()
+{
+//    freopen("tests/00", "r", stdin);
+    cin >> a >> b >> c;
+    cout << (c - a) / b;
+
+    return 0;
+}
