1a96d950ab53ea086e9fdfcbc92e9c34baf955fc
Aleksander <operations.rannsoknir@gmail.com>
2019-11-16 14:33:10 +0300

    hse/2020/lvl1/B

--- hse/2020/lvl1/B/simple.cpp
+++ hse/2020/lvl1/B/simple.cpp
@@ -1,30 +1,44 @@
 #include <iostream>
-#include <map>
+#include <cstring>
 using namespace std;
 #define X first
 #define Y second
 
+int n, k, smax;
+pair<int, int> *a, *b;
 
-int n, k;
-int **a;
+void print(pair<int, int> *c){
+    for (int i = 0; i < n; i++)
+        cout << c[i].X << " " << c[i].Y << "\n";
+}
 
-int main()
-{
-    freopen("tests/00", "r", stdin);
-    cin >> n >> k;
-    multimap<int, pair<int, int>> m;
-    a = new int*[2];
-    a[0] = new int[n];
-    a[1] = new int[n];
-    for (int i = 0; i < n; i++){
-        cin >> a[0][i] >> a[1][i];
-        m.insert({a[0][i], {0, i}});
-        m.insert({a[1][i], {1, i}});
+void f(int p, int q, int s, pair<int, int> *c){
+
+    for (int i = p; i <= k; i++){
+        c[i].X = c[i].Y = q;
+        s += (a[i].X + a[i].Y);
+        if (s > smax)
+            memcpy(b, c, sizeof(pair<int, int>) * n), smax = s;
+        print(c);
+        cout << s << " " << smax << "\n\n";
+        f(i + 1, q+1, s, c);
+        c[i].X = c[i].Y = 0;
+        s -= a[i].X + a[i].Y;
     }
-    int **c = new int*[2];
-    c[0] = new int[n]{};
-    c[1] = new int[n]{};
-    for (multimap<int, pair<int, int>>::iterator it = m.begin(); it != m.end(); ++it)
-        cout << it.X << " " << it.Y.X << " " << it.Y.Y << "\n";
+}
+
+int main(){
+    freopen("tests/02", "r", stdin);
+    cin >> n >> k;
+    a = new pair<int, int>[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i].X >> a[i].Y;
+
+    b = new pair<int, int>[n];
+    pair<int, int> *c = new pair<int, int>[n];
+    f(0, 1, 0, c);
+
+    for (int i = 0; i < n; i++)
+        cout << b[i].X << " " << b[i].Y << "\n";
     return 0;
 }
