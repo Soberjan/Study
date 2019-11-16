4f7ebb144fcf55f380947c16e92ae06f9cab5702
Aleksander <operations.rannsoknir@gmail.com>
2019-11-16 22:10:05 +0300

    hse/2020/lvl1/B/simple

--- hse/2020/lvl1/B/simple.cpp
+++ hse/2020/lvl1/B/simple.cpp
@@ -5,25 +5,63 @@ using namespace std;
 #define Y second
 
 int n, k, smax;
-pair<int, int> *a, *b;
+pair<int, int> *a, *b, *c;
 
 void print(pair<int, int> *c){
     for (int i = 0; i < n; i++)
         cout << c[i].X << " " << c[i].Y << "\n";
 }
 
-void f(int p, int q, int s, pair<int, int> *c){
+//void f(int p, int q, int s){
+//    for (int i = p; (i < n) && (q <= k); i++){
+//
+//        c[i].X = c[i].Y = q;
+//        s += (a[i].X + a[i].Y);
+//        if (s > smax)
+//            memcpy(b, c, sizeof(pair<int, int>) * n), smax = s;
+//        print(c);
+//        cout << s << " " << smax << "\n\n";
+//        f(i + 1, q+1, s);
+//        c[i].X = c[i].Y = 0;
+//        s -= a[i].X + a[i].Y;
+//    }
+//}
 
-    for (int i = p; i <= k; i++){
-        c[i].X = c[i].Y = q;
-        s += (a[i].X + a[i].Y);
-        if (s > smax)
-            memcpy(b, c, sizeof(pair<int, int>) * n), smax = s;
-        print(c);
-        cout << s << " " << smax << "\n\n";
-        f(i + 1, q+1, s, c);
-        c[i].X = c[i].Y = 0;
-        s -= a[i].X + a[i].Y;
+void f(int p, int q, int s){
+    for (int i = p; (i < n) && (q <= k); i++){
+        if ((c[i].X == 0) && (c[i].Y == 0)){
+            c[i].X = c[i].Y = q;
+            s += (a[i].X + a[i].Y);
+            if (s > smax)
+                memcpy(b, c, sizeof(pair<int, int>) * n), smax = s;
+            print(c);
+            cout << s << " " << smax << "\n\n";
+            f(i + 1, q + 1, s);
+            c[i].X = c[i].Y = 0;
+            s -= (a[i].X + a[i].Y);
+        }
+        if ((i + 1 < n) && (c[i].Y == 0)){
+            c[i].Y = c[i+1].Y = q;
+            s += (a[i].Y + a[i+1].Y);
+            if (s > smax)
+                memcpy(b, c, sizeof(pair<int, int>) * n), smax = s;
+            print(c);
+            cout << s << " " << smax << "\n\n";
+            f(i + 1, q + 1, s);
+            c[i].Y = c[i+1].Y = 0;
+            s -= (a[i].Y + a[i+1].Y);
+        }
+        if ((i + 1 < n) && (c[i].X == 0)){
+            c[i].X = c[i+1].X = q;
+            s += (a[i].X + a[i+1].X);
+            if (s > smax)
+                memcpy(b, c, sizeof(pair<int, int>) * n), smax = s;
+            print(c);
+            cout << s << " " << smax << "\n\n";
+            f(i + 1, q + 1, s);
+            c[i].X = c[i+1].X = 0;
+            s -= (a[i].X + a[i+1].X);
+        }
     }
 }
 
@@ -35,8 +73,8 @@ int main(){
         cin >> a[i].X >> a[i].Y;
 
     b = new pair<int, int>[n];
-    pair<int, int> *c = new pair<int, int>[n];
-    f(0, 1, 0, c);
+    c = new pair<int, int>[n];
+    f(0, 1, 0);
 
     for (int i = 0; i < n; i++)
         cout << b[i].X << " " << b[i].Y << "\n";
