e283ea07630edd088a9f0b7b0449c6cd19c2b83f
Aleksander <operations.rannsoknir@gmail.com>
2019-11-17 13:27:56 +0300

    C

--- hse/2020/lvl1/B/optimal.cpp
+++ hse/2020/lvl1/B/optimal.cpp
@@ -3,9 +3,9 @@ using namespace std;
 #define X first
 #define Y second
 #define ll long long
-#define f(i, j) (i * n) + j
+#define f(i, j) (i * (n+1)) + j
 
-struct quartet{ int a[4] = {0, 0, 0, 0}; };
+struct quartet{ pair<ll, int> a[4]; };;
 
 int n, k;
 pair<int, int> *p;
@@ -13,7 +13,7 @@ quartet *b;
 void print(){
     for (int i = 0; i < k + 1; i++){
         for (int j = 0; j < n + 1; j++)
-            printf("%3d:%3d:%3d:%3d |", b[f(i, j)].a[0], b[f(i, j)].a[1], b[f(i, j)].a[2], b[f(i, j)].a[3]);
+            printf("%3d:%3d:%3d:%3d |", (int)b[f(i, j)].a[0].X, (int)b[f(i, j)].a[1].X, (int)b[f(i, j)].a[2].X, (int)b[f(i, j)].a[3].X);
         printf("\n");
     }
     printf("\n");
@@ -22,31 +22,27 @@ void print(){
 int main(){
     freopen("tests/00", "r", stdin);
     cin >> n >> k;
-    p = new pair<int, int>[n+1];
-    for (int i = 1; i <= n; i++)
+    p = new pair<int, int>[n];
+    for (int i = 0; i < n; i++)
         cin >> p[i].X >> p[i].Y;
 
     b = new quartet[(k+1) * (n+1)];
-    print();
-    for (int i = 1; i <= k; i++)
-        for (int j = i; j < n; j++){
+    for (int i = 2; i <= k; i++)
+        for (int j = i + 1; j <= n; j++){
             for (int q = 0; q < 4; q++)
-                b[f(i, j)].a[0] = max(b[f(i, j)].a[0], b[f(i, j-1)].a[q]);
+                b[f(i, j)].a[0] = max(b[f(i, j)].a[0], make_pair(b[f(i, j-1)].a[q].X, q));
 
-            int x = p[j+1].Y + p[j].Y;
-            b[f(i, j)].a[1] = max(b[f(i, j)].a[1], x + b[f(i-1, j)].a[2]);
-            b[f(i, j)].a[1] = max(b[f(i, j)].a[1], x + b[f(i-1, j-1)].a[2]);
-            b[f(i, j)].a[1] = max(b[f(i, j)].a[1], x + b[f(i-1, j-1)].a[0]);
-            //print();
-            x = p[j+1].X + p[j].X;
-            b[f(i, j)].a[2] = max(b[f(i, j)].a[2], x + b[f(i-1, j)].a[1]);
-            b[f(i, j)].a[2] = max(b[f(i, j)].a[2], x + b[f(i-1, j-1)].a[1]);
-            b[f(i, j)].a[2] = max(b[f(i, j)].a[2], x + b[f(i-1, j-1)].a[0]);
+//            int x = p[i-1].Y + p[i-2].Y;
+//            b[f(i, j)].a[1] = max(make_pair(x + b[f(i-1, j-1)].a[2].X, 0), make_pair(x + b[f(i-1, j-1)].a[0].X, 1));
+//
+//            x = p[i-1].X + p[i-2].X;
+//            b[f(i, j)].a[2] = max(make_pair(x + b[f(i-1, j-1)].a[1].X, 0), make_pair(x + b[f(i-1, j-1)].a[0].X, 1));
 
-            b[f(i, j)].a[3] = p[j+1].X + p[j+1].Y + b[f(i-1, j)].a[0];
+            int x = p[i-1].X + p[i-1].Y;
+            b[f(i, j)].a[3] = make_pair(x + b[f(i-1, j)].a[0].X, 0);
         }
-    print();
 
+    print();
 
     return 0;
 }
