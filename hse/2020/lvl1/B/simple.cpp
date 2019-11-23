ec21e9143d72d653c06c6a466c0c0c48c9278282
Aleksander <operations.rannsoknir@gmail.com>
2019-11-23 17:54:41 +0300

    hse/2020/lvl1/B

--- hse/2020/lvl1/B/simple.cpp
+++ hse/2020/lvl1/B/simple.cpp
@@ -9,20 +9,20 @@ using namespace std;
 int n, k;
 vector<int> a, b;
 
-ll f(int i, int j, int h){
-    if (j == 0)
-        return 0;
-    set<int> s;
+ll f(int i, int j, int h);
+pair<ll, int> g(int i, int j, int h) {
+    set<pair<ll, int>> s;
     if (n - i > j)
-        s.insert(f(i+1, j, 0));
+        s.insert({f(i + 1, j, 0), 0});
     if ((n - i > j) && (h != 1))
-        s.insert(b[i] + b[i+1] + f(i+1, j-1, 1));
+        s.insert({b[i] + b[i+1] + f(i + 1, j - 1, 1), 1});
     if ((n - i > j) && (h != 2))
-        s.insert(a[i] + a[i+1] + f(i+1, j-1, 2));
+        s.insert({a[i] + a[i+1] + f(i + 1, j - 1, 3), 2});
     if ((h != 1) && (h != 2))
-        s.insert(a[i] + b[i] +   f(i+1, j-1, 3));
+        s.insert({a[i] + b[i]   + f(i + 1, j - 1, 3), 3});
     return *(--s.end());
 }
+ll f(int i, int j, int h) { return j > 0 ? g(i, j, h).X : 0; }
 
 int main(){
     freopen("tests/03", "r", stdin);
@@ -31,7 +31,30 @@ int main(){
     for (int i = 0; i < n; i++)
         cin >> a[i] >> b[i];
 
-    cout << f(0, k, 0);
+    pair<int,int> *r = new pair<int, int>[n] {};
+    for (int i = 0, j = k, h = 0, m = 0; j > 0; i++)
+        switch (h = g(i, j, h).Y) {
+            case 1: --j; r[i].Y = r[i+1].Y = ++m; break;
+            case 2: --j; r[i].X = r[i+1].X = ++m; break;
+            case 3: --j; r[i].X =   r[i].Y = ++m; break;
+        }
 
+    for (int i = 0; i < n; i++)
+        cout << r[i].X << " " << r[i].Y << "\n";
     return 0;
 }
+
+//ll f(int i, int j, int h){
+//    if (j == 0)
+//        return 0;
+//    set<int> s;
+//    if (n - i > j)
+//        s.insert(f(i+1, j, 0));
+//    if ((n - i > j) && (h != 1))
+//        s.insert(b[i] + b[i+1] + f(i+1, j-1, 1));
+//    if ((n - i > j) && (h != 2))
+//        s.insert(a[i] + a[i+1] + f(i+1, j-1, 2));
+//    if ((h != 1) && (h != 2))
+//        s.insert(a[i] + b[i] +   f(i+1, j-1, 3));
+//    return *(--s.end());
+//}
