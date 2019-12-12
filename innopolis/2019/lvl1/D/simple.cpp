a34a814660e8978980433decbe551973bc9f6611
Aleksander <operations.rannsoknir@gmail.com>
2019-12-12 17:45:30 +0300

    innopolis/2019/lvl1/D рефакторинг

--- innopolis/2019/lvl1/D/simple.cpp
+++ innopolis/2019/lvl1/D/simple.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define X first
 #define Y second
 
+const ll INF = 1e17;
 int n, s, v;
 int *a;
 
@@ -15,15 +16,13 @@ ll idx(const trio& l){ return l.i * (v+1) * (s+1) + l.j * s + l.q; }
 bool operator<(const trio& l, const trio& r){ return idx(l) < idx(r); }
 
 ll f(int i, int j, int q);
-pair<ll, int> g(int i, int j, int q){
-//    cout << i << " " << j << " " << q << "\n";
+pair<ll, int> g(int i, int j, int h){
     if (j == 0)
         return {0, 0};
-    set<pair<ll, int>> m;
-    for (int k = 0; (k <= j) && (k <= v); k++)
-        if ( j - k <= v * (n - i) )
-            m.insert({q * k * a[i-1] + f(i+1, j-k, k), k});
-    return *m.begin();
+    pair<ll, int> m = {INF, INF};
+    for (int q = max(0, j - (n - i - 1) * v); q <= min(j, v); q++)
+        m = min(m, {h * a[i] * q + f(i + 1, j - q, q), q});
+    return m;
 }
 ll f(int i, int j, int q) {
     trio t{i, j, q};
@@ -31,20 +30,14 @@ ll f(int i, int j, int q) {
 }
 
 int main(){
-    freopen("tests/01", "r", stdin);
+    freopen("tests/00", "r", stdin);
     cin >> n >> s >> v;
     a = new int[n+1];
-    a[0] = a[n] = 0;
     for (int i = 1; i < n; i++)
         cin >> a[i];
 
-    int *b = new int[n];
-    for (int i = 1, j = s, q = 0; i <= n; i++){
-        b[i-1] = q = g(i, j, q).Y;
-        j -= q;
-    }
+    for (int i = 0, j = s, h = 0; i < n; i++, j -= h)
+        cout << (h = g(i,j,h).Y) << " ";
 
-    for (int i = 0; i < n; i++)
-        cout << b[i] << " ";
     return 0;
 }
